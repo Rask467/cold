4d8356895a854a0e1da16357328f2d6855d5799c
Rask467 <conner.jensen@hey.com>
2023-03-09 10:20:41 -0700

    Add subscription account

---
+++ src/SubscriptionAccount.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "account-abstraction/core/BaseAccount.sol";
+
+contract SubscriptionAccount is BaseAccount, UUPSUpgradeable, Initializable {}
